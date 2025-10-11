84dd7d28b14304245908f8991ad92fe874681749
jinesh <jineshbasnet6@gmail.com>
2025-10-11 19:17:14 +0545

    craco config

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,6 +2,8 @@ module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       if (webpackConfig.devServer) {
+        webpackConfig.devServer.onAfterSetupMiddleware = undefined;
+        webpackConfig.devServer.onBeforeSetupMiddleware = undefined;
         webpackConfig.devServer.setupMiddlewares = (middlewares, devServer) => {
           if (!devServer) {
             throw new Error('webpack-dev-server is not defined');
