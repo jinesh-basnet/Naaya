e201baee44d0b7a6d974c8a3f6f9e5452b481da3
stack-nova <jineshbasnet6@gmail.com>
2026-03-10 11:46:55 +0545

    enhance nav

--- frontend/src/utils/navItems.ts
+++ frontend/src/utils/navItems.ts
@@ -1,5 +1,5 @@
 import { IconType } from 'react-icons';
-import { FaHome, FaVideo, FaCompass, FaUser, FaCog, FaSignOutAlt} from 'react-icons/fa';
+import { FaHome, FaVideo, FaCompass, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa';
 
 export interface NavItem {
   icon: IconType;
@@ -9,6 +9,13 @@ export interface NavItem {
 }
 
 export const getNavItems = (t: (key: string) => string): NavItem[] => [
+  { icon: FaHome, labelKey: 'nav.home', path: '/home' },
+  { icon: FaCompass, labelKey: 'nav.explore', path: '/explore' },
+  { icon: FaVideo, labelKey: 'nav.reels', path: '/reels' },
+  { icon: FaUser, labelKey: 'nav.profile', path: '/profile' },
+];
+
+export const getBottomNavItems = (t: (key: string) => string): NavItem[] => [
   { icon: FaHome, labelKey: 'nav.home', path: '/home' },
   { icon: FaCompass, labelKey: 'nav.explore', path: '/explore' },
   { icon: FaVideo, labelKey: 'nav.reels', path: '/reels' },
