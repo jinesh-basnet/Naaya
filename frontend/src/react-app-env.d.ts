5969fdc09291e50737336b2afc7b6caffd8ec571
stack-nova <jineshbasnet6@gmail.com>
2026-04-11 23:15:48 +0545

    finalize environment type declarations and project config

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.less";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
+declare module "*.webp";
